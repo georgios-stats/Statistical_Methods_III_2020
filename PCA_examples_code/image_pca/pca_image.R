63a354ad29ada984d2eb5cdc485156ae630e977e
georgios-stats <georgios.stats@gmail.com>
2021-04-22 11:19:13 +0100

    4

--- PCA_examples_code/image_pca/pca_image.R
+++ PCA_examples_code/image_pca/pca_image.R
@@ -1,5 +1,29 @@
-# Georgios Karagiannis
-
+# /*
+# * Copyright (C) 2021 'Georgios P. Karagiannis' <georgios.stats@gmail.com>
+# *
+# * This program is free software: you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation, either version 3 of the License.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# *
+# *
+# * Author details
+# *
+# * Georgios P. Karagiannis
+# * Department of Mathematical Sciences
+# * Durham University, Durham, UK
+# *
+# * Email: <georgios.karagiannis@durham.ac.uk>
+# *
+# * Contact email: <georgios.stats@gmail.com>
+# */
 
 rm(list=ls())
 #install.packages('jpeg')
